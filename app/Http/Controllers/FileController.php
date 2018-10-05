aacb9974c791cce208b5d1e4ccf11d35a2e812d4
Victoria Bodnar <bodnarvik@gmail.com>
2018-10-05 12:51:23 +0300

    a bit change function checkFile

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -48,8 +48,8 @@ class FileController extends Controller
             if($data->count()){
                 
                 $result_checking=$this->checkFile($data);
-                return $result_checking;
-                if ($result_checking!="OK") return $result_checking;
+                //return $result_checking;
+                if ($result_checking!="OK") { //return $result_checking;
 
                 foreach ($data as $key => $value) {
                     $arr[] = ['kod_consumer' => $value->kod_consumer, 'date_zamer' => $value->date_zamer, 'type_zamer' => $value->type_zamer, 'a1' => $value->a1, 'a2' => $value->a2, 'a3' => $value->a3, 'a4' => $value->a4, 'a5' => $value->a5, 'a6' => $value->a6, 'a7' => $value->a7, 'a8' => $value->a8, 'a9' => $value->a9, 'a10' => $value->a10, 'a11' => $value->a11, 'a12' => $value->a12, 'a13' => $value->a13, 'a14' => $value->a14, 'a15' => $value->a15, 'a16' => $value->a16, 'a17' => $value->a17, 'a18' => $value->a18, 'a19' => $value->a19, 'a20' => $value->a20, 'a21' => $value->a21, 'a22' => $value->a22, 'a23' => $value->a23, 'a24' => $value->a24, 'a_cyt' => $value->a_cyt, 'user_id' => Auth::user()->id,'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s") ];
@@ -59,6 +59,9 @@ class FileController extends Controller
                     \DB::table('grafs')->insert($arr);
                     dd('Insert Record successfully.');
                 }
+            }else{
+                return $result_checking;
+            }
             }
             //return $arr[0]['kod_consumer']." ---  ".$arr[0]['type_zamer']."***";
         }
@@ -80,8 +83,8 @@ class FileController extends Controller
                 if (!$paspsRow)  return view('file_import_export',['data' => $data, 'message' => 'Undefined date_zamer:  '.$value->date_zamer, 'errRow' => $value->kod_consumer]);
 
                 $typesRow = \DB::table('types')->where('name_type', $value->type_zamer)->first();
-                return view('file_import_export',['data' => $data, 'message' => 'type_zamer:  '.$value->type_zamer, 'errRow' => $value->kod_consumer]);
-                //if (!$typesRow)  return view('file_import_export',['data' => $data, 'message' => 'Undefined type_zamer:  '.$value->type_zamer, 'errRow' => $value->kod_consumer]);
+                //return view('file_import_export',['data' => $data, 'message' => 'type_zamer:  '.$value->type_zamer, 'errRow' => $value->kod_consumer]);
+                if (!$typesRow)  return view('file_import_export',['data' => $data, 'message' => 'Undefined type_zamer:  '.$value->type_zamer, 'errRow' => $value->kod_consumer]);
             }
             
             //check kod_consumer for each row of the uploading file. It must be in consumers table.
@@ -122,7 +125,7 @@ class FileController extends Controller
         //check uniqueness kod_consumer of the uploading file.
         if (count($onlyKodConsumer)!=count(array_unique($onlyKodConsumer))) {
             //return "kod_consumer is not unique in uploading file";
-            return view('file_import_export',['data' => $data, 'message' => 'kod_consumer is not unique in uploading file:  '.$value->kod_consumer, 'errRow' => $value->kod_consumer]);
+            return view('file_import_export',['data' => $data, 'message' => 'kod_consumer is not unique in an uploading file:  '.$value->kod_consumer, 'errRow' => $value->kod_consumer]);
         }
         //echo '<pre>',print_r($onlyKodConsumer,true),'</pre>';
         //echo '<pre>',$c1,'*************************',$c2,'</pre>';
