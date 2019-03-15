7eb12b748d8dafa844ff742f5d13446cedc8735c
Victoria Bodnar <bodnarvik@gmail.com>
2019-03-15 16:16:33 +0200

    checks done.file successfuly load into DB

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -22,12 +22,12 @@ class FileController extends Controller
         //return view('file_import_export');
          return view('file_import_export', ['data' => [], 'message' => '']);
     }
-     public function importFileIntoDB(Request $request){
-        /*if($request->hasFile('sample_file_graf')){
+    public function importFileIntoDB(Request $request){
+       /* if($request->hasFile('file_for_upload')){
              return "yes i have a file";
          }
          return "nofile";*/
-         //return "it's importfileintoBD!!!";
+         //return "it's importfileintoBD!!!".$request;
 
         if($request->hasFile('file_for_upload')){
             $path = $request->file('file_for_upload')->getRealPath();
@@ -49,15 +49,20 @@ class FileController extends Controller
                 
                 $result_checking=$this->checkFile($data);
                 //return $result_checking;
-                if ($result_checking!="OK") { //return $result_checking;
+                
+                if ($result_checking > 0) { 
 
                 foreach ($data as $key => $value) {
                     $arr[] = ['kod_consumer' => $value->kod_consumer, 'date_zamer' => $value->date_zamer, 'type_zamer' => $value->type_zamer, 'a1' => $value->a1, 'a2' => $value->a2, 'a3' => $value->a3, 'a4' => $value->a4, 'a5' => $value->a5, 'a6' => $value->a6, 'a7' => $value->a7, 'a8' => $value->a8, 'a9' => $value->a9, 'a10' => $value->a10, 'a11' => $value->a11, 'a12' => $value->a12, 'a13' => $value->a13, 'a14' => $value->a14, 'a15' => $value->a15, 'a16' => $value->a16, 'a17' => $value->a17, 'a18' => $value->a18, 'a19' => $value->a19, 'a20' => $value->a20, 'a21' => $value->a21, 'a22' => $value->a22, 'a23' => $value->a23, 'a24' => $value->a24, 'a_cyt' => $value->a_cyt, 'user_id' => Auth::user()->id,'created_at' => date("Y-m-d H:i:s"), 'updated_at' => date("Y-m-d H:i:s") ];
                 }
 
                 if(!empty($arr)){
+                    \DB::table('grafs')->delete();///THIS ROW NEED DEIETE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
                     \DB::table('grafs')->insert($arr);
-                    dd('Insert Record successfully.');
+                    //dd('Insert Record successfully.');
+                    
+                    return view('grafs');
+                    //return 'grafs';
                 }
             }else{
                 return $result_checking;
@@ -80,16 +85,16 @@ class FileController extends Controller
             if ($i==1){
 
                 $paspsRow = \DB::table('pasps')->where('date_zamer', $value->date_zamer)->first();//'2003-12-17'
-                if (!$paspsRow)  return view('file_import_export',['data' => $data, 'message' => 'Undefined date_zamer:  '.$value->date_zamer, 'errRow' => $value->kod_consumer]);
+                if (!$paspsRow)  return view('file_import_export',['data' => $data, 'message' => 'Знайдено невідому дату вимірів:  '.$value->date_zamer, 'errRow' => $value->kod_consumer]);
 
                 $typesRow = \DB::table('types')->where('name_type', $value->type_zamer)->first();
                 //return view('file_import_export',['data' => $data, 'message' => 'type_zamer:  '.$value->type_zamer, 'errRow' => $value->kod_consumer]);
-                if (!$typesRow)  return view('file_import_export',['data' => $data, 'message' => 'Undefined type_zamer:  '.$value->type_zamer, 'errRow' => $value->kod_consumer]);
+                if (!$typesRow)  return view('file_import_export',['data' => $data, 'message' => 'Знайдено невідомий тип вимірів:  '.$value->type_zamer, 'errRow' => $value->kod_consumer]);
             }
             
             //check kod_consumer for each row of the uploading file. It must be in consumers table.
             $consumersRow = \DB::table('consumers')->where('kod_consumer', $value->kod_consumer)->first();//
-            if (!$consumersRow) return view('file_import_export',['data' => $data, 'message' => 'Undefined kod_consumer:  '.$value->kod_consumer, 'errRow' => $value->kod_consumer]);
+            if (!$consumersRow) return view('file_import_export',['data' => $data, 'message' => 'Невідомий код споживача:  '.$value->kod_consumer, 'errRow' => $value->kod_consumer]);
 
 
             //check if value of measure is NUMERIC and INTEGER 
@@ -101,11 +106,11 @@ class FileController extends Controller
                 $num_r_for_message=$i+1;
                 if (!$pos === false) {
                     //return "!!!!!!!!!!!!!!!!!Period found!!!!!!!!!!!!!!!!!!!!!!".$v."=".$vv." in row ".$num_r_for_message;
-                    return view('file_import_export',['data' => $data, 'message' => '!!!!!!!!!!!!!!!!!Period found!!!!!!!!!!!!!!!!!!!!!!'.$v.'='.$vv.' in row '.$num_r_for_message, 'errRow' => $value->kod_consumer]);
+                    return view('file_import_export',['data' => $data, 'message' => '!!!!!!!!!!!!!!!!!Знайдене не ціле число!!!!!!!!!!!!!!!!!!!!!!'.$v.'='.$vv.' in row '.$num_r_for_message, 'errRow' => $value->kod_consumer]);
                 }
                 if (!is_numeric($vv)) {
                     //return "!!!!!!!!!!!!НЕЧИСЛО!!!!!!!!!!!!!!!!!!!!!!!!!!!".$v."=".$vv." in row ".$num_r_for_message;
-                    return view('file_import_export',['data' => $data, 'message' => '!!!!!!!!!!!!НЕЧИСЛО!!!!!!!!!!!!!!!!!!!!!!!!!!!'.$v.'='.$vv.' in row '.$num_r_for_message, 'errRow' => $value->kod_consumer]);
+                    return view('file_import_export',['data' => $data, 'message' => '!!!!!!!!!!!!Знайдене нечислове значення виміру!!!!!!!!!!!!!!!!!!!!!!!!!!!'.$v.'='.$vv.' in row '.$num_r_for_message, 'errRow' => $value->kod_consumer]);
                 }
                 if ($v=='a_cyt'){
                     if ($checkSumCyt!=$vv) { return view('file_import_export',['data' => $data, 'message' => '!!!!!!!!!!!!НЕВІРНЕ ДОБОВЕ ЗНАЧЕННЯ!!!!!!!!!!!!!!!!!!!!!!!!!!!'.$vv.'----------------'.$checkSumCyt.' in row '.$num_r_for_message, 'errRow' => $value->kod_consumer]);} //return "!!!!!!!!!!!!НЕВІРНЕ ДОБОВЕ ЗНАЧЕННЯ!!!!!!!!!!!!!!!!!!!!!!!!!!!".$vv."----------------".$checkSumCyt." in row ".$num_r_for_message;}
@@ -129,8 +134,10 @@ class FileController extends Controller
         }
         //echo '<pre>',print_r($onlyKodConsumer,true),'</pre>';
         //echo '<pre>',$c1,'*************************',$c2,'</pre>';
-        echo "checkSumFile = ".$checkSumFile."<br>";
-        return "Uploading file is OK ";
+        //echo "Завантаження даних успішне. Контрольна сума = ".$checkSumFile."<br>";
+        //return "Uploading file is OK ";
+        //return view('welcome');
+        return $checkSumFile;
     }    
     
    
